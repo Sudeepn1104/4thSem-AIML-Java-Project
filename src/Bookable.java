12dd94cd416ba714c915f5775dcbc50b76067c6c
Savin <savinmoolya14@gmail.com>
2026-06-02 19:13:18 +0530

    Delete src directory

--- src/Bookable.java
+++
@@ -1,7 +0,0 @@
-public interface Bookable {
-    Ticket bookTicket(User user, Match match, int numberOfSeats, Payment payment);
-
-    boolean cancelTicket(String ticketId);
-
-    void viewTicket(String ticketId);
-}
